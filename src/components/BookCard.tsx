6732a17d3423891c6b755dfe516a6f9b8b51cca6
hikio-17 <fajri010217@gmail.com>
2024-01-25 00:41:12 +0700

    fix: fixed book card

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -3,14 +3,14 @@ import CustomButton from "./CustomButton";
 
 function BookCard() {
    return (
-      <div className='book-card group'>
-         <img src="harry-potter.jpg" alt="book" className="rounded-3xl w-full" style={{ height: '60%' }} />
+      <div className='book-card group h-fit'>
+         <img src="harry-potter.jpg" alt="book" className="rounded-3xl w-full" style={{ height: '350px' }} />
          <div className="px-3 mt-3">
             <h2 className="book-card__price">$ 30, 00</h2>
             <h5 className="book-card__content-title">Harry Potter and the Chamber of Secrets</h5>
          </div>
-         <div className="relative flex w-full mt-10 ">
-            <div className="flex group-hover:invisible w-full justify-between text-gray px-7">
+         <div className="relative flex w-full my-5 ">
+            <div className="flex group-hover:invisible w-full justify-between text-gray px-5">
                <div className="flex flex-col justify-center items-center gap-2">
                   <IoBookOutline />
                   <p className='text-[14px]'>
@@ -31,7 +31,7 @@ function BookCard() {
                </div>
             </div>
 
-            <div className="book-card__btn-container">
+            <div className="book-card__btn-container px-5">
                <CustomButton
                   title='View More'
                   containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
